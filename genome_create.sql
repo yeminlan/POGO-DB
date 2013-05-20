7f3cb0b76af6e399034883179654c27093dc343c
Calvin <calvin@EESI>
2013-05-20 11:03:33 -0400

    tables

---
+++ genome_create.sql
@@ -0,0 +1,14 @@
+delimiter $$
+
+CREATE TABLE `Genome` (
+  `idSpecies` int(11) NOT NULL AUTO_INCREMENT,
+  `Genome Name` varchar(256) DEFAULT NULL,
+  `Phylum` varchar(256) DEFAULT NULL,
+  `Class` varchar(256) DEFAULT NULL,
+  `Order` varchar(256) DEFAULT NULL,
+  `Family` varchar(256) DEFAULT NULL,
+  `Genus` varchar(256) DEFAULT NULL,
+  `Species` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`idSpecies`)
+) ENGINE=InnoDB AUTO_INCREMENT=2015 DEFAULT CHARSET=big5$$
+
