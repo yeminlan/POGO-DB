c09a7f3b4a03b6496609aa33902073e0b853049d
Calvin <calvin@EESI>
2013-06-05 11:19:09 -0400

    taxonomy sql update

--- sql/taxonomycsv2sql.sh
+++ sql/taxonomycsv2sql.sh
@@ -3,5 +3,5 @@ cat taxonomy.csv |
 sed '1d' |
 sed "s/[^,]*/'&'/g" | 
 sed "s/'NA'/NULL/g" | 
-sed "s/^/INSERT INTO Taxonomy VALUES(NULL, /" |
+sed "s/^/INSERT INTO taxonomy VALUES(NULL, /" |
 sed "s/$/);/" > taxonomy.sql
