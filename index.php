f81b629965d5fdb8ca16096f4b59c9999d5a3bbf
Calvin <calvin@EESI>
2013-06-05 11:05:55 -0400

    more renaming

--- index.php
+++ index.php
@@ -1,127 +1,101 @@
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="default.css">
+<title>PID</title>
+</head>
+
+<body>
+<div class="header">
+<form action="index.php" method="get" style="margin-bottom:0em;">
+Search For: <input type="text" name="search">
+In: <select name="taxonomy">
+<option selected value="All">All</option>
+<option value="Genome">Genome</option>
+<option value="Phylum">Phylum</option>
+<option value="Class">Class</option>
+<option value="Order">Order</option>
+<option value="Family">Family</option>
+<option value="Genus">Genus</option>
+<option value="Species">Species</option>
+</select>
+<input type="submit">
+</form>
+</div>
+<div class='results'>
 
 <?php
 
 include 'login.php';
-include 'search.php';
-error_reporting(E_ALL);
+ini_set('display_errors', 'On');
 
-$query = "SELECT Genus, Species, Genome, idGenome FROM taxonomy;";
-$result = mysqli_query($con, $query);
-$taxonomy=array();
 
-while($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
-  $taxonomy[] = $row;
+if(isset($_GET["id1"]) && isset($_GET["id2"])) {
+
+  $query = "SELECT * FROM data WHERE genome_id1='" . $_GET["id1"] . "' AND genome_id2='" . $_GET["id2"] . "';";
+  echo "<h3>Comparing " . $_GET["id1"] . " with " . $_GET["id2"] . "</h3>";
+
+  $result = mysqli_query($con, $query);
+  $row = mysqli_fetch_array($result);
+  $row_names = mysqli_fetch_fields($result);
+
+  for($i = 0; $i < mysqli_field_count($con); $i++) {
+    echo $row_names[$i]->name . ": ". $row[$i] . "<br>";
+  }
 }
 
-$query = "SELECT distinct Genus FROM taxonomy;";
-$result = mysqli_query($con, $query);
-$genus=array();
+else if(isset($_GET["id"])) {
+  $query = "SELECT genome_id1, genome_id2 FROM data WHERE genome_id1='" . $_GET["id"] . "';";
+  $result = mysqli_query($con, $query);
 
-while($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
-  $genus[] = $row['Genus'];
+  echo "<h3>Select a genome to compare with " . $_GET["id"] . "</h3>";
+
+  while($row = mysqli_fetch_array($result)) {
+    echo "<a href='index.php?id1=" . $row['genome_id1'] . "&id2=" . $row['genome_id2'] . "'>" . $row['genome_id2'] . "</a><br>";
+  }
 }
 
-$query = "SELECT distinct Species FROM taxonomy;";
-$result = mysqli_query($con, $query);
-$species=array();
+else if(isset($_GET["search"])) {
+  $query = "SELECT * FROM taxonomy WHERE `" . $_GET["taxonomy"] . "` LIKE '%" . $_GET["search"] . "%';";
 
-while($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
-  $species[] = $row['Species'];
+  if($_GET["taxonomy"] === "All") {
+  $query = "SELECT * FROM taxonomy WHERE ";
+  $query = $query . "`Genome` LIKE '%" . $_GET["search"] . "%' ";
+  $query = $query . "OR `Phylum` LIKE '%" . $_GET["search"] . "%' ";
+  $query = $query . "OR `Class` LIKE '%" . $_GET["search"] . "%' ";
+  $query = $query . "OR `Order` LIKE '%" . $_GET["search"] . "%' ";
+  $query = $query . "OR `Family` LIKE '%" . $_GET["search"] . "%' ";
+  $query = $query . "OR `Genus` LIKE '%" . $_GET["search"] . "%' ";
+  $query = $query . "OR `Species` LIKE '%" . $_GET["search"] . "%';";
+  }
+
+  echo "<h3>Searching " . $_GET["taxonomy"] . " for " . $_GET["search"] . "</h3>";
+
+  $result = mysqli_query($con, $query);
+  while($row = mysqli_fetch_array($result)) {
+    echo "<a href='index.php?id=" . $row['Genome'] . "'>" . $row['Genome'] . "</a><br>";
+  }
 }
 
-$query = "SELECT distinct Genome FROM taxonomy;";
-$result = mysqli_query($con, $query);
-$genome=array();
-
-while($row = mysqli_fetch_array($result, MYSQL_ASSOC)) {
-  $genome[] = $row['Genome'];
+else {
+  echo "<h2>Welcome to PID</h2>";
+  $query = "SELECT * FROM taxonomy;";
+  $result = mysqli_query($con, $query);
+  while($row = mysqli_fetch_array($result)) {
+    echo "<a href='index.php?id=" . $row['Genome'] . "'>" . $row['Genome'] . "</a><br>";
+  }
 }
+
+mysqli_close($con);
+
+
+echo "</div><div class='footer'>";
+if(isset($result)) {
+  printf("Query returned %d results<br>", mysqli_num_rows($result));
+} else {
+  echo "<br>";
+}
+echo "</div>";
+
 ?>
-
-<link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css">
-<link rel="stylesheet" type="text/css" href="default.css">
-
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<script type="text/javascript" src="//datatables.net/download/build/jquery.dataTables.nightly.js"></script>
-<script type="text/javascript" src="pid.js"></script>
-
-<title>PID</title>
-
-</head>
-
-<body>
-
-<?php include 'header.php'; ?>
-<center>
-<h2>Available Genomes</h2>
-<table style="left-margin:10%;right-margin:10%;"> 
-<tr>
-<script type="text/javascript" charset="utf8">
-  var taxonomy = <?php echo json_encode($taxonomy); ?>;
-  var genus = <?php echo json_encode($genus); ?>;
-  var species = <?php echo json_encode($species); ?>;
-  var genome = <?php echo json_encode($genome); ?>;
-
-</script>
-
-<td>
-  Genus:<br>
-  <select onChange="selected('Genus')" id='Genus' size=20></select>
-  <center>
-    <p>
-      <button onClick="add('Genus', 'A')" id='GenusAddA'>Add Genus to A</button>
-      <button onClick="add('Genus', 'B')" id='GenusAddBr'> Add Genus to B</button>
-    </p>
-  </center>
-</td>
-
-<td>
-  Species:<br>
-  <select onChange="selected('Species')" id='Species' size=20></select>
-  <center>
-    <p>
-      <button onClick="add('Species', 'A')" id='SpeciesAddA'>Add Species to A</button>
-      <button onClick="add('Species', 'B')" id='SpeciesAddBr'> Add Species to B</button>
-    </p>
- </center>
-</td>
-
-<td>Genome:<br>
-  <select id='Genome' size=20></select>
-  <center>
-    <p>
-      <button onClick="add('Genome', 'A')"id='GenomeAddA'>Add Genome to A</button>
-      <button onClick="add('Genome', 'B')" id='GenomeAddBr'> Add Genome to B</button>
-    </p>
-  </center>
-</td>
-
-<script type="text/javascript">
-  populate();
-</script>
-</td>
-</table>
-
-<p>
-<h3>Selected Items</h3>
-</p>
-
-<h4>A Items</h4>
-<table class="display" id="BoxA"><thead><tr><th>Genus</th><th>Species</th><th>Genome</th></tr></thead></td></tr><tbody></tbody></table><br>
-<button "removeA" onClick="rm('#BoxA')">Remove Selected Item from A</button>
-<button "removeAllA" onClick="clearTable('#BoxA')">Remove All from A</button>
-
-<h4>B Items</h4>
-<table class="display" id="BoxB"><thead><tr><th>Genus</th><th>Species</th><th>Genome</th></tr></thead></td></tr><tbody></tbody></table><br>
-<p>
-<button id="removeB" onClick="rm('#BoxB')">Remove Selected Item from B</button>
-<button "removeAllB" onClick="clearTable('#BoxB')">Remove All from B</button>
-<div class="buttonGroup">
-<button id="clearAll" onClick="clearTable('#BoxA');clearTable('#BoxB')">Reset Comparison</button>
-<button id="submit" onClick="submit()">Submit Query</button>
-</div>
-</center>
 </body>
 </html>
