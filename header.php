49aaa3be2e49ba52d3b029cfbde14a7185625816
Calvin <calvin@EESI>
2013-07-02 09:32:33 -0400

    close anchor tag

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <div class="header">
 <div class="h_logo"><img src="images/drexel.jpg" alt="drexel logo" height=50 width=50 /> </div>
-<div class="h_title"><a href="/">POGO db</div>
+<div class="h_title"><a href="/">POGO db</a></div>
 <div class="h_link"><a href="about.php">About</a></div>
 <div class="h_link"><a href="faq.php">F.A.Q.</a></div>
 <div class="h_link"><a href="http://www.ece.drexel.edu/gailr/EESI/">EESI Lab</a></div>
