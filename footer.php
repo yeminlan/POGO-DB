d92c13b511b8698aabc24d15498215bab76fc01b
Calvin <calvin@EESI>
2013-07-05 13:36:34 -0400

    add legalese

--- footer.php
+++ footer.php
@@ -3,6 +3,8 @@
   <div class="centered">
     <center>
     &copy; Drexel University 2013
+    <br>
+    <p style="font-size:60%">This material is based upon work supported by the National Science Foundation under Grant Number (#). Any opinions, findings, and conclusions or recommendations expressed in this material are those of the author(s) and do not necessarily reflect the views of the National Science Foundation.</p>
     </center>
   </div>
 </div>
