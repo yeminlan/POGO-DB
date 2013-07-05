72579630c3bda7dbb367f5e03819f0b2844a5b3f
Calvin <calvin@EESI>
2013-07-05 11:59:32 -0400

    added contact page

---
+++ contact.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> POGO Contact</title>
+  <link rel="stylesheet" type="text/css" href="default.css">
+</head>
+<div id=container>
+<?php include 'header.php'; ?>
+<div class=main> 
+<h1>Contact.</h1>
+please email <a href="mailto:eesi.pogo@gmail.com">eesi.pogo@gmail.com</a> with any questions or comments.
+</div>
+<?php include 'footer.php'; ?>
+</div>
+</body>
+</html>
