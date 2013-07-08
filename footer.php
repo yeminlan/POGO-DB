1e3b99dcb2d7ffed066550fd080154a03311cb78
Calvin <calvin@EESI>
2013-07-08 15:39:48 -0400

    updated legalse

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
     <center>
     &copy; Drexel University 2013
     <br>
-    <p style="font-size:60%">This material is based upon work supported by the National Science Foundation under Grant Number (#). Any opinions, findings, and conclusions or recommendations expressed in this material are those of the author(s) and do not necessarily reflect the views of the National Science Foundation.</p>
+    <p style="font-size:60%">This website is based upon work supported by the National Science Foundation (NSF) under Grant No. DBI-0845827 and under Department of Energy (DOE) award DE-SC0004335. Any opinions, findings, and conclusions or recommendations expressed in this website do not necessarily reflect the views of the NSF or DOE.</p>
     </center>
   </div>
 </div>
