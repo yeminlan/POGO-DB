283697584b3586a0ec533e3f81e23debdb7c7675
Calvin Morrison <mutantturkey@gmail.com>
2013-07-29 12:14:49 -0400

    added gene ID support, and refactored our variables for clarity

--- download.php
+++ download.php
@@ -1,5 +1,8 @@
 <?php
 
+function lookup_genome($genome) {
+}
+
 include 'login.php';
 
 ini_set('display_errors', 'On');
@@ -30,25 +33,47 @@ if($result = mysqli_query($con, $query)) {
   $data=array();
 
   $prefix = "/data/pogo_db/blast_results/";
-  $csv_file = tempnam("/tmp", "pogo");
+  $ids_file = "data/geneIDs.txt";
 
-  $command = "tar -cf - --transform='s,.*tmp/pogo.*,/files.csv,;s,.*/,blast_files/,' " ;
+  $blast_files_file = tempnam("/tmp", "pogo-csv");
+  $gene_ids_file = tempnam("/tmp", "pogo-blast");
 
-  $csv_handle = fopen($csv_file, 'w');
-  fwrite($csv_handle, "Genome 1, Genome 2, Download Id, filename1v2, filename2v1\n");
+  $tar_command = "tar -cf - --transform='s,.*tmp/pogo-blast.*,/gene_ids.csv,;s,.*tmp/pogo-csv.*,/files.csv,;s,.*/,blast_files/,' " ;
+  $grep_command = "egrep '";
 
+  $blast_files_handle = fopen($blast_files_file, 'w');
+  $gene_ids_handle = fopen($gene_ids_file, 'w');
+
+  fwrite($blast_files_handle, "Genome 1, Genome 2, Download Id, filename1v2, filename2v1\n");
+  fwrite($gene_ids_handlee, shell_exec("sed -n 1p " . $ids_file));
+  $genome_list = array();
+  
 
   while($row = mysqli_fetch_array($result, MYSQL_NUM)) {
-    $command .= $prefix . $row[1] . " " . $prefix . $row[2] . " "; 
-    fwrite($csv_handle, $row[3] . ", " . $row[4] . ", " . $row[0] . ", " . preg_replace(",.*/,", "",  $row[1]) . ", " . preg_replace(",.*/,", "", $row[2]) . "\n");
+    $tar_command .= $prefix . $row[1] . " " . $prefix . $row[2] . " "; 
+    fwrite($blast_files_handle
+  , $row[3] . ", " . $row[4] . ", " . $row[0] . ", " . preg_replace(",.*/,", "",  $row[1]) . ", " . preg_replace(",.*/,", "", $row[2]) . "\n");
     $data[] = $row;
+      if(!in_array($row[3], $genome_list))
+        array_push($genome_list, $row[3]);
+      if(!in_array($row[4], $genome_list))
+        array_push($genome_list, $row[4]);
   }
+  
+  for($i = 0; $i < count($genome_list) - 1; $i++) {
+    $grep_command = $grep_command . $genome_list[$i] . "|";
+  }
+  $grep_command = $grep_command . $genome_list[count($genome_list) -1] . "' ";
 
-  fclose($csv_handle);
+  fwrite($gene_ids_handlee, shell_exec($grep_command . $ids_file));
 
-  $command .= " " . $csv_file;
 
-  if($tar_handle = popen($command, 'r')) {
+  fclose($blast_files_handle);
+  fclose($gene_ids_handlee);
+  
+  tar_command .= " " . $blast_files_file . " " . $gene_ids_file;
+
+  if($tar_handle = popen($tar_command, 'r')) {
     header("Content-Type: application/x-tar");
     header("Content-disposition: attachment; filename=blast_files.tar");
 
@@ -62,10 +87,11 @@ if($result = mysqli_query($con, $query)) {
     pclose($tar_handle);
   }
   else {
-    echo $command . "<br>";
+    echo $tar_command . "<br>";
     echo "something went wrong";
   }
-  unlink($csv_file);
+  unlink($blast_files_file);
+  unlink($gene_ids_file);
 } 
 else {
   echo $query . "<br>";
