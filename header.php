a147772bdfe292d9f2928d296e7968c116474e76
Calvin Morrison <mutantturkey@gmail.com>
2013-08-01 15:11:21 -0400

    add link to home in header

--- header.php
+++ header.php
@@ -2,9 +2,10 @@
 <div class="header">
 <div class="h_logo"><a href="http://www.ece.drexel.edu/gailr/EESI/"><img src="images/drexel.jpg" alt="EESI Lab" height=50 width=50 /></a></div>
 <div class="h_logo"><a href="http://hershberg.net.technion.ac.il/"><img src="images/technion_logo.png" alt="technicon logo" height=50 width=50 /></a></div>
-<div class="h_title"><a href="index.php">POGO db</a></div>
+<div class="h_title"><a href="/">POGO db</a></div>
 </div>
 <div class="h_links">
+  <div class="h_link"><a href="index.php">Home</a></div>
   <div class="h_link"><a href="about.php">About</a></div>
   <div class="h_link"><a href="faq.php">F.A.Q.</a></div>
   <div class="h_link"><a href="contact.php">Contact</a></div>
